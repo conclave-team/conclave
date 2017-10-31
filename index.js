2203ccf18fd9473137db6d41f161efaf9ecc25b5
Nitin Savant <nitin@Nitins-MacBook-Air.local>
2017-10-31 11:03:30 -0500

    Initial ExpressJS setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
