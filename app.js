5d7be4ff2042e2b5ff9169c4e8c82bf0a3a730b5
Nitin Savant <nitin@Nitins-MacBook-Air.local>
2017-11-03 00:07:04 -0500

    Passed GET request's host as a parameter to the controller instead of hardcoding localhost

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
+  const host = req.protocol + '://' + req.get('host');
   const id = req.query.id ? req.query.id : 0;
-  res.render('index', { id: id });
+  res.render('index', { id: id, host: host });
 });
 
 app.listen(3000, function () {
