c88655cf371e38ada1a6a1931bb6076efec9584c
Sunny <sjbeatteay@gmail.com>
2017-11-04 16:24:59 -0400

    add UUID to controller

---
+++ spec/broadcastSpec.js
@@ -0,0 +1,16 @@
+// import Broadcast from '../lib/broadcast';
+// import Peer from 'peerjs';
+//
+// describe('Broadcast', () => {
+//   const mockController = {
+//
+//   };
+//
+//   describe('constructor', () => {
+//     const broadcast = new Broadcast(mockController);
+//
+//     it('creates an instance of PeerJS', () => {
+//       expect(broadcast.peer instanceof Peer).toBe(true);
+//     });
+//   });
+// });
