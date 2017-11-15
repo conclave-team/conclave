e47375c12579ce93f4bc5a38dab32b556ecadf6c
Sun-Li Beatteay <sjbeatteay@gmail.com>
2017-11-15 15:09:38 -0500

    demo wip

---
+++ demo/demo.js
@@ -0,0 +1,23 @@
+import Peer from 'peerjs';
+import SimpleMDE from 'simplemde';
+
+import Controller from './controller';
+import Broadcast from './broadcast';
+import Editor from './editor';
+
+new Controller(
+  new Peer('conclavedemo', {host: 'conclavepeerjs.herokuapp.com', port: 443, secure: true, key: 'peerjs' , debug: 1}),
+  new Broadcast(),
+  new Editor(new SimpleMDE({
+    placeholder: "Share the link to invite collaborators to your room.",
+    spellChecker: false,
+    toolbar: false,
+    autofocus: true,
+    indentWithTabs: true,
+    tabSize: 4,
+    indentUnit: 4,
+    lineWrapping: false,
+    shortCuts: []
+  })),
+  []
+);
