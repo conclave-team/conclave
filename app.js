6b92d1b345d50cc1e2d7e7ac083fc82fc8c53c97
Sun-Li Beatteay <sjbeatteay@gmail.com>
2017-11-15 21:43:04 -0500

    create userbot

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-// const server = require('http').createServer(app);
 let listener;
 
 app.use(express.static('public'));
