3a42d8d88367c60be3c2206810ce958bade27e57
Sunny Beatteay <sbeatteay@digitalocean.com>
2021-06-07 09:17:45 -0400

    update view engine path

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-let listener;
 
 app.use(express.static('public'));
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
